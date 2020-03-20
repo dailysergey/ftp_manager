7d822bba3a8a9cabe57af2307e3a4fc09a4bc5ba
dailysergey <ds@ru>
2020-03-20 11:07:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ftp_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
